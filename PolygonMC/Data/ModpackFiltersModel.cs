de4b2c454ddc0a31e1e7a57b01c788f7602f8863
Drew Chase <dcmanproductions@gmail.com>
2023-08-01 01:07:50 -0400

    added filter

---
+++ PolygonMC/Data/ModpackFiltersModel.cs
@@ -0,0 +1,19 @@
+﻿/*
+    PolygonMC - LFInteractive LLC. 2021-2024
+    PolygonMC is a free and open source Minecraft Launcher implementing various modloaders, mod platforms, and minecraft authentication.
+    PolygonMC is protected under GNU GENERAL PUBLIC LICENSE version 3.0 License
+    https://www.gnu.org/licenses/gpl-3.0.en.html#license-text
+    https://github.com/DcmanProductions/PolygonMC
+*/
+
+namespace PolygonMC.Models;
+
+public sealed class ModpackFiltersModel
+{
+    public bool ShowCurseForge { get; set; }
+    public bool ShowModrinth { get; set; }
+    public bool ShowFabric { get; set; }
+    public bool ShowForge { get; set; }
+    public bool ShowQuilt { get; set; }
+    public List<string> Categories { get; set; } = new List<string>();
+}
