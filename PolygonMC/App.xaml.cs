3d9036baf5ea953beecdc7d497bf065cfff753a8
Drew Chase <dcmanproductions@gmail.com>
2023-07-31 20:59:43 -0400

    Initial Commit

---
+++ PolygonMC/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace PolygonMC;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ PolygonMC/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace PolygonMC;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
