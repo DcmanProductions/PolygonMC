6e5149f980b9e166db8bc04674eae9b5e4cae082
Drew Chase <dcmanproductions@gmail.com>
2023-08-06 19:29:08 -0400

    added console window

---
+++ PolygonMC/ConsolePage.xaml.cs
@@ -0,0 +1,9 @@
+namespace PolygonMC;
+
+public partial class ConsolePage : ContentPage
+{
+	public ConsolePage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ PolygonMC/ConsoleWindow.cs
@@ -0,0 +1,29 @@
+/*
+    PolygonMC - LFInteractive LLC. 2021-2024
+    PolygonMC is a free and open source Minecraft Launcher implementing various modloaders, mod platforms, and minecraft authentication.
+    PolygonMC is protected under GNU GENERAL PUBLIC LICENSE version 3.0 License
+    https://www.gnu.org/licenses/gpl-3.0.en.html#license-text
+    https://github.com/DcmanProductions/PolygonMC
+*/
+
+namespace PolygonMC;
+
+public partial class ConsoleWindow : Application
+{
+    public ConsoleWindow()
+    {
+        MainPage = new ConsolePage();
+    }
+
+    protected override Window CreateWindow(IActivationState activationState)
+    {
+        Window window = base.CreateWindow(activationState);
+
+        window.Title = "Console - PolygonMC";
+        window.Width = window.MinimumWidth = 800;
+        window.Height = window.MinimumHeight = 480;
+        window.Y = window.X = 50;
+
+        return base.CreateWindow(activationState);
+    }
+}
