3fb5eed125072bd00b9ceaa10a65f9f975100741
Drew Chase <dcmanproductions@gmail.com>
2023-08-11 16:44:11 -0400

    added Local Resource Manager

---
+++ PolygonMC/Data/LocalResourceManager.cs
@@ -0,0 +1,13 @@
+﻿/*
+    PolygonMC - LFInteractive LLC. 2021-2024
+    PolygonMC is a free and open source Minecraft Launcher implementing various modloaders, mod platforms, and minecraft authentication.
+    PolygonMC is protected under GNU GENERAL PUBLIC LICENSE version 3.0 License
+    https://www.gnu.org/licenses/gpl-3.0.en.html#license-text
+    https://github.com/DcmanProductions/PolygonMC
+*/
+
+namespace PolygonMC.Data;
+
+public static class LocalResourceManager
+{
+}
